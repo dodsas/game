e337709af8fe5405d16c80fcb1d8f0be53a3ceed
Yousun <dods87@naver.com>
2022-12-23 08:40:52 +0900

    remove backup

--- backup/robot.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-def pressKey(key: str, sleep: float = 2, duration: float = 1):
-    pyautogui.sleep(sleep)
-    print('pressKey: ' + key)
-    pyautogui.keyDown(key)
-    pyautogui.sleep(duration)
-    pyautogui.keyUp(key)
