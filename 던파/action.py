6536a7ea19782e43fdca1ae7f146854dc5ff1843
남유선 <dodsas@nam-yuseon-ui-MacBookAir.local>
2022-12-29 10:42:16 +0900

    add home macbook progress

--- 던파/action.py
+++ 던파/action.py
@@ -73,9 +73,10 @@ def 캐릭터선택(char:Unit):
         if(pt != None):
             imageFinder.findAndClick('캐릭_' + char.name, threshold=0.80, sleep=0, error=False)
             break
-        pyautogui.scroll(-1000)
+        pyautogui.scroll(-80000)
         pyautogui.sleep(0.1)
     imageFinder.findAndClick('캐릭_게임시작')
+    pyautogui.sleep(3)
 
 def 산등최초입장():
     imageFinder.findAndClick('확인', sleep=9, error=False)
@@ -88,7 +89,8 @@ def 산등최초입장():
 def 산등노가다(char:Unit):
 
     for j in range(20):
-        pyautogui.sleep(3)
+        # pyautogui.sleep(3)
+        pyautogui.sleep(1.8)
         pyautogui.press(str(char.buffIndex))
         imageFinder.findAndClick('산등맵', 1, 0.75, error=False)
         if(imageFinder.isFound('지옥파티', 0.5) != None):
@@ -98,7 +100,7 @@ def 산등노가다(char:Unit):
         for i in range(300):
             print(i)
             # if multip 30
-            if(i%30 == 0):
+            if(i%30 == 0 and i != 0):
                 robot.pressKey('right', duration=2)
                 imageFinder.findAndClick('부활', 1, 0.75, error=False)
 
@@ -161,20 +163,21 @@ def 신비상점구매(char:Unit):
     robot.pressKey('ESC')
 
 def 크리처():
-    robot.pressKey('ESC', sleep=7)
+    robot.pressKey('ESC', sleep=5)
     imageFinder.findAndClick('크리처')
 
     imageFinder.findAndClick('크리처_심부름')
     if(imageFinder.isFound('크리처_보상받기', sleep=4) != None):
         imageFinder.findAndClick('크리처_보상받기')
         imageFinder.findAndClick('확인')
-    imageFinder.findAndClick('크리처_빠른심부름', sleep=7)
-    imageFinder.findAndClick('크리처_자동배치')
-    imageFinder.findAndClick('크리처_보내기')
-    if(imageFinder.isFound('확인', sleep=4) == None):
-        robot.pressKey('ESC')
-    else:
-        imageFinder.findAndClick('확인')
+    if(imageFinder.isFound('크리처_빠른심부름', sleep=7) != None):
+        imageFinder.findAndClick('크리처_빠른심부름', sleep=0)
+        imageFinder.findAndClick('크리처_자동배치')
+        imageFinder.findAndClick('크리처_보내기')
+        if(imageFinder.isFound('확인', sleep=4) == None):
+            robot.pressKey('ESC')
+        else:
+            imageFinder.findAndClick('확인')
     robot.pressKey('ESC')
 
 def 아티팩트판매():
--- 던파/기본사냥.py
+++ 던파/기본사냥.py
@@ -8,41 +8,46 @@ import os
 
 # map of Unit objects
 map = {
-   "보리뚜": Unit("보리뚜", 3, 'q', 신비전체구매=True, 산등노가다=True, 길드기부=True),
-   "보리성": Unit("보리성", 3, 'w', 신비전체구매=True, 산등노가다=True, 길드기부=True),
-   "보리세이더": Unit("보리세이더", 3, 'w', 신비전체구매=True, 산등노가다=True, 길드기부=True),
-   "보리빵떡": Unit("보리빵떡", 3, 'w', 신비전체구매=True, 산등노가다=True, 길드기부=True),
-   "윈드꾸꾸": Unit("윈드꾸꾸", 6, 'w', False),
-   "보리뚜킥": Unit("보리뚜킥", 3, 'w', False),
-   "보리커": Unit("보리커", 3, 'w', False),
-   "보리뚜비": Unit("보리뚜비", 3, 'w', False),
-   "보리꾸꾸": Unit("보리꾸꾸", 6, 'w', True),
-   "런처꾸꾸": Unit("런처꾸꾸", 6, 'w', True),
-   "보리템플러": Unit("보리템플러", 3, 'w', True),
-   "보리술사": Unit("보리술사", 3, 'w', True),
-   "소울뚜": Unit("소울뚜", 3, 'w', True),
-   "보리뚜뚜": Unit("보리뚜뚜", 6, 'w', True),
-   "웨펀꾸꾸": Unit("웨펀꾸꾸", 3, 'w', False),
+#    "보리뚜": Unit("보리뚜", 3, 'q', 신비전체구매=True, 산등노가다=True, 길드기부=True),
+#    "보리성": Unit("보리성", 3, 'w', 신비전체구매=True, 산등노가다=True, 길드기부=True),
+#   "보리세이더": Unit("보리세이더", 3, 'w', 신비전체구매=True, 산등노가다=False, 길드기부=True),
+#   "보리빵떡": Unit("보리빵떡", 3, 'w', 신비전체구매=True, 산등노가다=False, 길드기부=True),
+#   "윈드꾸꾸": Unit("윈드꾸꾸", 6, 'w', False),
+#   "보리뚜킥": Unit("보리뚜킥", 3, 'w', False),
+#   "보리커": Unit("보리커", 3, 'w', False),
+#    "보리뚜비": Unit("보리뚜비", 3, 'w', False),
+  "보리꾸꾸": Unit("보리꾸꾸", 6, 'w', True),
+  "런처꾸꾸": Unit("런처꾸꾸", 6, 'w', True),
+  "보리템플러": Unit("보리템플러", 3, 'w', True),
+  "보리술사": Unit("보리술사", 3, 'w', True),
+  "소울뚜": Unit("소울뚜", 3, 'w', True),
+  "보리뚜뚜": Unit("보리뚜뚜", 6, 'w', True),
+  "웨펀꾸꾸": Unit("웨펀꾸꾸", 3, 'w', False),
     "베인뚜": Unit("베인뚜", 6, 'w', True),
     "보리핏": Unit("보리핏", 3, 'w', False),
+    "보리파": Unit("보리파", 3, 'w', False),
 }
 
 # romove all files in imagesLog folder on osx
 os.system('rm -rf imagesLog/*')
 
-# map iterator
 for key in map:
     char = map[key]
     action.캐릭터선택(char)
-    action.크리처()
 
     if(char.산등노가다):
         action.산등최초입장()
         action.산등노가다(char)
         action.수리및보관()
 
+    action.크리처()
     action.길드활동(char)
     action.친구포인트()
     action.즐찾구매()
     action.신비상점구매(char)
     action.아티팩트판매()
+
+# for key in map:
+#     char = map[key]
+#     action.캐릭터선택(char)
+#     action.크리처()
